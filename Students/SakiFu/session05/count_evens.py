80a5ddacd2180c8d592cd550df504f76c4d27801
Saki Fu <sakiukaji@Sakis-MacBook-Air.local>
2014-12-18 13:57:46 -0800

    Edited count_evens.py

--- Students/SakiFu/session05/count_evens.py
+++ Students/SakiFu/session05/count_evens.py
@@ -1,3 +1,9 @@
 
 def count_evens(nums):
-    return len([x for x in range(len(nums)) if not x % 2])
+    return len([x for x in nums if not x % 2])
+
+if __name__ == '__main__':
+    assert count_evens([2, 1, 2, 3, 4]) == 3
+    assert count_evens([2, 2, 0]) == 3
+    assert count_evens([1, 3, 5]) == 0
+    print 'All Tests Pass'
