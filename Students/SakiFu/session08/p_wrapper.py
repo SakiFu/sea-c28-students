9ae3c196b7294f6492a340238f0d12a048e01d6d
Saki Fu <sakifu.86@gmail.com>
2014-12-24 12:35:21 -0800

    Edited p_wrapper.py

--- Students/SakiFu/session08/p_wrapper.py
+++ Students/SakiFu/session08/p_wrapper.py
@@ -1,7 +1,8 @@
-
-def p_wrapper(a_function):
-    def new_function(string):
-        return "<p>{0}</p>".format(string)
+def p_wrapper(func):
+    def new_function(*args, **kwargs):
+        string = func(*args, **kwargs)
+        string = "<p>{0}</p>".format(string)
+        return string
     return new_function
 
 
@@ -9,6 +10,21 @@ def p_wrapper(a_function):
 def return_string(string):
     return string
 
+
+@p_wrapper
+def string_multiplier(string):
+    return string * 5
+
+
+@p_wrapper
+def string_b(string):
+    return "<b>{0}</b>".format(string)
+
+
 if __name__ == '__main__':
     assert return_string("I'm wrapped by a p tag.") ==\
         "<p>I'm wrapped by a p tag.</p>"
+
+    assert string_multiplier("I'm string")
+
+    assert string_b("I'm a b tag and a p tag.")
