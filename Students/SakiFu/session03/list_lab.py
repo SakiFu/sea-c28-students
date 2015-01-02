2ada99433eaa135f41e99f1c6b29250d9dd12cdc
Saki Fu <sakifu.86@gmail.com>
2015-01-01 21:31:32 -0800

    Edited list_lab.py

--- Students/SakiFu/session03/list_lab.py
+++ Students/SakiFu/session03/list_lab.py
@@ -9,9 +9,9 @@ fruit = raw_input("What is your favorite fruit? ")
 fruits.append(fruit)
 print fruits
 #Ask the user for a number
-num = raw_input("What is your favorite number? ")
+num = int(raw_input("What is your favorite number? "))
 #Display the number and the fruit corresponding to that number.
-print fruits[int(num)]
+print fruits[int(num - 1)]
 print fruits
 #Add another fruit to the beginning of the list using “+”.
 fruits = ["Kiwis"] + fruits
@@ -21,7 +21,7 @@ fruits.insert(0, "Mangos")
 print fruits
 #Display all the fruits that begin with “P”, using a for loop.
 for i in fruits:
-    if i[0] == "P":
+    if i[0].lower() == "p":
         print i
     else:
         pass
@@ -32,19 +32,25 @@ del fruits[-1]
 print fruits
 #Ask the user for a fruit to delete and find it and delete it.
 dislike = raw_input("Which fruits you don't like?")
-fruits.remove(dislike)
+while dislike in fruits:
+    fruits.remove(dislike)
+
+print fruits
 
 for i in fruits[:]:
-    answer = raw_input("Do you like %s ? " % i.lower())
-    while (answer != "No") and (answer != "Yes"):
-        answer == raw_input("Yes or No")
-    if answer == "No":
-        fruits.remove(i)
-    elif answer == "Yes":
-        continue
+    answer = raw_input("Do you like %s ? " % i)
+    if answer.lower() == "no":
+        while i in fruits:
+            fruits.remove(i)
+    elif answer.lower() == "yes":
+        pass
+    else:
+        print "Yes or No"
 
-fruits_copy = fruits[:]
-fruits_copy = fruits_copy[::-1]
-del fruits[-1]
+#Make a copy of the list and reverse the letters in each fruit.
+copy_fruits = []
+for i in fruits:
+    copy_fruits.append(i[::-1])
+
 print fruits
-print fruits_copy
+print copy_fruits
